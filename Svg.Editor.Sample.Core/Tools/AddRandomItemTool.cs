139a91875aea2d7107d4b73bc53f5ce533b930db
Alexander Marek <gentledepp@gmail.com>
2016-06-23 01:10:56 +0200

    Fixed boundingbox calculation of elements which have children

--- Svg.Editor.Sample.Core/Tools/AddRandomItemTool.cs
+++ Svg.Editor.Sample.Core/Tools/AddRandomItemTool.cs
@@ -23,11 +23,12 @@ namespace Svg.Droid.SampleEditor.Core.Tools
                 {
                     if (SourceProvider == null)
                         return;
-                    var provider = SourceProvider("isolib/Welds/solid/weld1.svg");
+                    //var provider = SourceProvider("isolib/Welds/solid/weld1.svg");
                     //var provider = SourceProvider("isolib/Valves/Valves/valve1.svg");
                     //var provider = SourceProvider("isolib/Valves/Valves/valve2.svg");
                     //var provider = SourceProvider("isolib/Valves/Valves/valve3.svg");
                     //var provider = SourceProvider("isolib/Valves/Valves/valve4.svg");
+                    var provider = SourceProvider("isolib/Reducers/solid/reducer1.svg");
                     //var provider = SourceProvider("isolib/Straights/solid and broken/solid1.svg");
                     //var provider = SourceProvider("svg/painting-control-01-f.svg");
                     var otherDoc = SvgDocument.Open<SvgDocument>(provider);
