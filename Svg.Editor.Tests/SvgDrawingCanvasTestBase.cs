05343e7ba9315b0dea7d447b6f2f5e5228cddcab
Alexander Marek <gentledepp@gmail.com>
2016-08-02 14:23:47 +0200

    Added shared editor tests for android and windows

---
+++ Svg.Editor.Tests/SvgDrawingCanvasTestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Svg.Core;
+
+namespace Svg.Editor.Core.Tests
+{
+    public abstract class SvgDrawingCanvasTestBase
+    {
+        private SvgDrawingCanvas _canvas;
+
+        protected SvgDrawingCanvas Canvas => _canvas;
+
+        static SvgDrawingCanvasTestBase()
+        {
+            SvgPlatformSetup.Init(new SvgSkiaPlatformOptions() {EnableFastTextRendering = true});
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _canvas = new SvgDrawingCanvas();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            _canvas.Dispose();
+        }
+    }
+}
---
+++ Svg.Editor.Tests/SvgDrawingCanvasTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Svg.Core.Tools;
+
+namespace Svg.Editor.Core.Tests
+{
+    [TestFixture]
+    public class SvgDrawingCanvasTests : SvgDrawingCanvasTestBase
+    {
+        [Test]
+        public void HasExplicitTool()
+        {
+            // Assert
+            var t = Canvas.ActiveTool;
+
+            Assert.IsNotNull(t);
+            Assert.AreEqual(ToolUsage.Explicit, t.ToolUsage);
+        }
+    }
+}
