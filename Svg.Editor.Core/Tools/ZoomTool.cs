3c00a15564a9264d6a6aedfbf5dc2bba07fa902f
Matthias Bruzek <mathias.bruzek@opti-q.com>
2016-08-25 11:28:06 +0200

    Add zoom to point documentation

--- Svg.Editor.Core/Tools/ZoomTool.cs
+++ Svg.Editor.Core/Tools/ZoomTool.cs
@@ -102,6 +102,80 @@ namespace Svg.Core.Tools
                     CurrentFocusY = se.FocusY;
                     if (!_focused)
                     {
+                        /*
+                         * A zoom with a focal point is a mix of scaling and translation. When the user zooms in, we will place the focal point on the canvas
+                         * coordinates that relate to the screen coordinates of the gesture and scale the canvas around that point. Scaling around the focal
+                         * point means effectively stretching all what's left of the focus to the left, all what's above to the top and so on. All what is
+                         * stretched over the bounds of the screen will be cut out by the renderer. If the focus was in the center of the screen, the objects
+                         * on the sides will exit when zooming in and enter when zooming out again, while objects in the middle stay centered. If the focus is
+                         * in the top left, the objects on the far right and bottom sides will exit when zooming in and enter when zooming out, and so on.
+                         * 
+                         * Let's assume the user wants to zoom to an object in the top left of the screen, and then zoom out from the screen center again. The
+                         * first zoom is executed just as described, the canvas is stretched to the right and bottom sides while the top left remains on screen.
+                         * Now, the focal point will change to the screen center, which means the scaling will happen around that point. Just changing the focus
+                         * will result in a jump to the new focus and the user losing sight of the objects in the top left, but we want the objects in the top
+                         * left to stay where they are until we actually change the zoom factor. To acheive this, we need to save this position by adding a
+                         * translation to the canvas. This translation has to cope for the jump between the two focal points, so we take the offset between them,
+                         * relative to the zoom factor we had at the previous zoom, and translate the canvas to the opposite. The process is explained graphically
+                         * with a canvas of the size 4*3.
+                         * 
+                         * 
+                         *     P..... previous focus
+                         *     F..... focal point
+                         *     p..... previous zoom factor
+                         *     z..... zoom facor
+                         *     
+                         *       a)   F = ?|?                   b)    F = 0|0                                      c)      F = 4|0
+                         *            z = 1                           z = 2                                                z = 2
+                         *     
+                         *       0                   4         0                                       8        -4                  0                    4
+                         *     0 +-------------------+       0 F-------------------+-------------------+       0 +------------------+--------------------F 
+                         *       |                   |         |                   |                   |         |                  |                    | 
+                         *       |                   |         |                   |                   |         |                  |                    | 
+                         *       |                   |         |                   |                   |         |                  |                    | 
+                         *       |                   |         |                   |                   |         |                  |                    | 
+                         *       |                   |         |                   |                   |         |                  |                    | 
+                         *     3 +-------------------+         +-------------------+                   |         |                  +--------------------+ 
+                         *                                     |                                       |         |                                       |
+                         *                                     |                                       |         |                                       |
+                         *                                     |                                       |         |                                       |
+                         *                                     |                                       |         |                                       |
+                         *                                   6 +---------------------------------------+       6 +---------------------------------------+
+                         *     
+                         * 
+                         * If the zoom factor was 1, we don't need to translate anything (a). If it was 2, the canvas was doubled in size and translated to a
+                         * proportion of the focus. If the focus was 0|0, we don't need to translate either (b). But if the focus was set to 4|0 (c), we need to
+                         * translate -4 on the x axis to save this translation.
+                         * 
+                         * 
+                         * 
+                         *        d)    F = 4|0                                                          e)      F = 0|0
+                         *              z = 3                                                                    z = 3
+                         *              P = 0|0                                                                  P = 4|0
+                         *              p = 2                                                                    p = 2
+                         *                                                                        
+                         *      -2         0                   4                             10       -6                             0                   4         6
+                         *     0 +---------P-------------------F-----------------------------+       0 +-----------------------------F-------------------P---------+
+                         *       |         |                   |                             |         |                             |                   |         |
+                         *       |         |                   |                             |         |                             |                   |         |
+                         *       |         |                   |                             |         |                             |                   |         |
+                         *       |         |                   |                             |         |                             |                   |         |
+                         *       |         |                   |                             |         |                             |                   |         |
+                         *       |         +-------------------+                             |         |                             +-------------------+         |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |                         
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *       |                                                           |         |                                                           |
+                         *     6 +-----------------------------------------------------------+       6 +-----------------------------------------------------------+
+                         * 
+                         * 
+                         */
                         var canvasFocus = ws.ScreenToCanvas(CurrentFocusX, CurrentFocusY);
                         ws.Translate -= (ws.ZoomFocus - canvasFocus) * (ws.ZoomFactor - 1);
                         ws.ZoomFocus = canvasFocus;
