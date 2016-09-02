8872588c698a069ff5f8ed865381dd028c3130e6
Matthias Bruzek <mathias.bruzek@opti-q.com>
2016-09-02 14:48:22 +0200

    Implement place image in background

---
+++ Svg.Editor.Sample.Core/Tools/AndroidPickImageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.Platform;
+using MvvmCross.Plugins.PictureChooser;
+using Svg.Core.Interfaces;
+using Svg.Interfaces;
+
+namespace Svg.Droid.SampleEditor.Core.Tools
+{
+    public class AndroidPickImageService : IPickImageService
+    {
+        public async Task<string> PickImagePath()
+        {
+            using (var inStream = await Mvx.Resolve<IMvxPictureChooserTask>().ChoosePictureFromLibrary(2048, 80))
+            {
+                var fs = Engine.Resolve<IFileSystem>();
+                var path = fs.PathCombine(fs.GetDefaultStoragePath(), $"{Guid.NewGuid()}.png");
+                using (var outStream = fs.OpenWrite(path))
+                {
+                    inStream.CopyTo(outStream);
+                }
+                return path;
+            }
+        }
+    }
+}
---
+++ Svg.Editor.Sample.Core/Tools/PlaceAsBackgroundTool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Svg.Core;
+using Svg.Core.Interfaces;
+using Svg.Core.Tools;
+
+namespace Svg.Droid.SampleEditor.Core.Tools
+{
+    public class PlaceAsBackgroundTool : UndoableToolBase
+    {
+        public PlaceAsBackgroundTool(string jsonProperties, IUndoRedoService undoRedoService) : base("PlaceAsBackground", jsonProperties, undoRedoService)
+        {
+            IconName = "ic_insert_photo_white_48dp.png";
+        }
+
+        public override Task Initialize(SvgDrawingCanvas ws)
+        {
+
+            Commands = new List<IToolCommand>
+            {
+                new ToolCommand(this, "Place background image", async o =>
+                {
+                    //var fs = Engine.Resolve<IFileSystem>();
+                    //var svgCachingService = Engine.Resolve<ISvgCachingService>();
+                    //var colorTool = Canvas.Tools.OfType<ColorTool>().Single();
+                    //var selectedColor = colorTool.SelectedColor;
+                    //var path = svgCachingService.GetCachedPngPath(colorTool.IconName, $"{selectedColor.R}_{selectedColor.G}_{selectedColor.B}", fs);
+                    var path = await new AndroidPickImageService().PickImagePath();
+                    try
+                    {
+                        Canvas.Document.Children.Insert(0, new SvgImage
+                        {
+                            Href = new Uri($"file://{path}", UriKind.Absolute)
+                        });
+                    }
+                    catch (IOException)
+                    {
+                        Debugger.Break();
+                    }
+                })
+            };
+
+            return base.Initialize(ws);
+        }
+    }
+}
