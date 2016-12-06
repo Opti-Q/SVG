a9519c1f7a2585841269a8df84103cffd2d65f19
Matthias Bruzek <mathias.bruzek@opti-q.com>
2016-12-06 10:53:21 +0100

    2125: Filler - Nuget script

---
+++ nuspec/pack.ps1
@@ -0,0 +1,16 @@
+$sourceNugetExe = "http://nuget.org/nuget.exe"
+$targetNugetExe = $PSScriptRoot + "\nuget.exe"
+
+if (-Not(Test-Path ("nuget.exe")))
+{
+	Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+}
+
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+del *.nupkg
+
+nuget pack Svg.nuspec -Symbols
+nuget pack Svg.Editor.nuspec -Symbols
+
+pause
