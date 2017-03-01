d3c1bd0b9b9f04ef2eb1df80bc9ad03a7ebab5e9
Alexander Marek <gentledepp@gmail.com>
2017-03-01 17:55:05 +0100

    fixed uwp renderer

---
+++ Svg.Editor.Forms.UWP/SKXamlCanvasX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp.Views.UWP;
+using Svg.Editor.Shared;
+
+namespace SkiaSharp.Views.UWP
+{
+    public class SKXamlCanvasX : SKXamlCanvas, IPaintSurface
+    {
+    }
+}
