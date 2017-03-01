2cfdcca5b309d6dfb0d0b024e10892dec721e77a
Matthias Bruzek <mathias.bruzek@opti-q.com>
2017-03-01 16:14:22 +0100

    Make build work

--- Svg.Editor.Forms/SvgCanvasEditorView.cs
+++ Svg.Editor.Forms/SvgCanvasEditorView.cs
@@ -1,9 +1,25 @@
-﻿using SkiaSharp.Views.Forms;
+﻿using System;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
 
 namespace Svg.Editor.Forms
 {
     public class SvgCanvasEditorView : SKCanvasView
     {
-        
+
+    }
+    public interface ISKCanvasViewController : IViewController
+    {
+        // the native listens to this event
+        event EventHandler SurfaceInvalidated;
+        event EventHandler<GetCanvasSizeEventArgs> GetCanvasSize;
+
+        // the native view tells the user to repaint
+        void OnPaintSurface(SKPaintSurfaceEventArgs e);
+    }
+    public class GetCanvasSizeEventArgs : EventArgs
+    {
+        public SKSize CanvasSize { get; set; }
     }
 }
--- Svg.Tests.Win/SaveDocumentTests.cs
+++ Svg.Tests.Win/SaveDocumentTests.cs
@@ -12,7 +12,6 @@ namespace Svg.Tests.Win
         [SetUp]
         public void SetUp()
         {
-            var setup = new SvgPlatformSetup();
         }
 
         [Test]
