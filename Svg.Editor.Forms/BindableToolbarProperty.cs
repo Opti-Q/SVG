66d59dceaa0b6da0bd26ab31c23f6fa624f603d9
Matthias Bruzek <mathias.bruzek@opti-q.com>
2017-03-02 17:58:48 +0100

    Add bindable toolbaritems attached property

---
+++ Svg.Editor.Forms/BindableToolbarProperty.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Svg.Editor.Forms
+{
+    public static class BindableToolbarProperty
+    {
+        public static readonly BindableProperty BindableToolbarItemsProperty = BindableProperty.CreateAttached("BindableToolbarItems",
+           typeof(List<ToolbarItem>),
+           typeof(Page),
+           new List<ToolbarItem>(),
+           propertyChanged: ToolbarItemsChanged);
+
+        private static void ToolbarItemsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var page = bindable as Page;
+
+            if (page == null) return;
+
+            if (oldValue != null)
+            {
+                page.ToolbarItems.Clear();
+            }
+
+            var items = newValue as List<ToolbarItem>;
+            if (items != null)
+            {
+                foreach (var item in items)
+                    page.ToolbarItems.Add(item);
+            }
+        }
+
+        public static List<ToolbarItem> GetBindableToolbarItems(BindableObject bindable)
+        {
+            return (List<ToolbarItem>) bindable.GetValue(BindableToolbarItemsProperty);
+        }
+
+        public static void SetBindableToolbarItems(BindableObject bindable, List<ToolbarItem> value)
+        {
+            bindable.SetValue(BindableToolbarItemsProperty, value);
+        }
+    }
+}
