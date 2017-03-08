ba87c644eb41c48612ab0f1f9d9934bbb02087d8
Alexander Marek <gentledepp@gmail.com>
2017-03-08 17:14:06 +0100

    created nuget packaging scripts

---
+++ Nuget/package_editor_views.ps1
@@ -0,0 +1,18 @@
+
+$rootPath = $pwd;
+$targetNugetExe = "$rootPath\nuget.exe"
+
+if(-not (Test-Path $targetNugetExe)){
+    $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+}
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+nuget pack Svg.Editor.Views.nuspec
+
+#nuget pack "..\Svg.Editor.Views.Droid\Svg.Editor.Views.Droid.csproj"
+#nuget pack "..\Svg.Editor.Views.Droid\Svg.Editor.Views.Droid.csproj" -Symbols
+#nuget pack "..\Svg.Editor.Views.iOS\Svg.Editor.Views.iOS.csproj"
+#nuget pack "..\Svg.Editor.Views.iOS\Svg.Editor.Views.iOS.csproj" -Symbols
+#nuget pack "..\Svg.Editor.Views.UWP\Svg.Editor.Views.UWP.csproj"
+#nuget pack "..\Svg.Editor.Views.UWP\Svg.Editor.Views.UWP.csproj" -Symbols
---
+++ Nuget/package_svg.ps1
@@ -0,0 +1,13 @@
+
+$rootPath = $pwd;
+$targetNugetExe = "$rootPath\nuget.exe"
+
+if(-not (Test-Path $targetNugetExe)){
+    $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+}
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+
+
+nuget pack Svg.nuspec
